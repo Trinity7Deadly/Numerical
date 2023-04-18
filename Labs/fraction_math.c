8cb272cafd180b041f8f2fcf51ea7e967a735f3a
Fionn <psychedover@gmail.com>
2023-04-18 00:49:09 -0700

    04-18-23 Late Night Labs

---
+++ Labs/fraction_math.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <complex.h>
+#include <string.h>
+
+// Define long int as lint for later use
+typedef long int
+lint ;
+
+// Create a structure called fraction for later use
+typedef struct {
+  lint a ;
+  lint b ;
+} fraction ;
+
+// Prints the fraction in a legible format
+void print_frac(char front[], fraction a, char back[]) {
+  printf("%s(%ld/%ld)%s", front, a.n, a.d, back) ;
+}
+
+// Checks if the fraction is zero
+int zero_frac(fraction a) {
+  if (a.n == 0 && a.d != 0) {
+    return 1 ;
+  } else {
+    return 0 ;
+  }
+}
+
+// Finds the greatest common denominator
+lint greatestCD(lint a, lint b) {
+  if (a == 0) {
+    return b ;
+    
+  } else if (b == 0) {
+    return a ;
+    
+  } else if (a > b) {
+    return greatestCD(a%b, b) ;
+    
+  } else {
+    return greatestCD(a, b%a) ;
+    
+  }
+}
+
+// Reduces the fractoin using the greatestCD function
+fraction reduce_frac(fraction a) {
+  fraction frac ;
+  lint temp ;
+
+  temp = greatestCD(labs(a.n), labs(a.d)) ;
+
+  frac.n = frac.n/temp ;
+  frac.d = frac.d/temp ;
+
+  if (r.d < 0) {
+    r.d = -r.d ;
+    r.n = -r.n ;
+  }
+
+  return frac ;
+}
+
+// Creates a fraction
+fraction create_frac(lint numerator, lint denominator) {
+  fraction frac ;
+  frac.n = numerator ;
+  frac.d = denominator ;
+  return reduce_frac(frac) ;
+}
+
+// Adds two fractions
+fraction add_frac(fraction a, fraction b) {
+  fraction tempA, tempB, temp ;
+
+  tempA.n = a.n * b.d;
+  tempA.d = a.d * b.d;
+
+  tempB.n = b.n * a.d;
+  tempB.d = ta.d;
+
+  temp.n = ta.n + tb.n;
+  temp.d = tb.d;
+
+  return reduce_frac(temp) ;
+}
+
+// Subtracts two fractions
+fraction subtract_frac(fraction a, fraction b) {
+  fraction temp;
+  tempB.n = -1 * b.n;
+  tempB.d = b.d;
+
+  return add_frac(a, tempB);
+}
+
+// Multiplies two fractions
+fraction mult_frac(fraction a, fraction b) {
+  fraction temp;
+
+  temp.n = a.n * b.n ;
+  temp.d = a.d * b.d ;
+
+  return reduce_frac(temp) ;
+}
+
+// Divides two fractions
+fraction divide_frac(fraction a, fraction b) {
+  fraction temp ;
+
+  temp.n = b.d ;
+  temp.d = b.n ;
+
+  return mult_frac(a, temp) ;
+}
+
+// Compares the fractions
+int compare_frac(fraction a, fraction b) {
+  fraction tempA, tempB, temp ;
+
+  tempA.n = a.n * b.d;
+  tempA.d = a.d * b.d;
+
+  tempB.n = b.n * a.d;
+  tempB.d = tempA.d;
+
+  if (tempA.n > tempB.n) {
+    return 1 ;
+  } else if (tempA.n < tempB.n) {
+    return -1 ;
+  } else {
+    return 0 ;
+  }
+}
--- Labs/midterm_equation_powers.c
+++ Labs/midterm_equation_powers.c
@@ -1,9 +1,132 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <complex.h>
-#include <string.h>
+#include "fraction_math.c"
 
+int MAX = 30 ;
+
+int printMatrix(fraction mat[L][L], int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n + 1; j++)
+        {
+            print_fraction("", mat[i][j], "");
+        }
+        printf("\n");
+    }
+}
+
+int solveMatrix(fraction mat[L][L], int n)
+{
+    printf("Sum(k^%d) = ", n-1);
+
+    fraction result[n+1];
+    for(int i = 0; i <= n; i++){
+        result[i] = make_fraction(0,1);
+    }
+    for (int i = n; i > 0; i--)
+    {
+        fraction sum = make_fraction(0, 1);
+        for (int j = 1; j <= n; j++)
+        {
+            fraction tfrac = frmul(result[j], mat[i][j]);
+            
+            sum = fradd(sum, tfrac);
+     
+        }
+        fraction t2 = frsub(mat[i][n + 1], sum);
+	
+        result[i] = frdiv(t2, mat[i][i]);
+        if(i !=  1){
+            print_fraction("", result[i], ""); printf("*n^%d + ", i);
+        }
+        else{
+            print_fraction("", result[i], ""); printf("*n^%d\n", i);
+        }
+        
+    }
+
+}
+
+int triangularize(fraction mat[L][L], int n)
+{
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+
+            fraction max = make_fraction(0, 1);
+            int loc = i;
+
+            for (int k = i; k <= n; k++)
+            {
+                if (frcomp(mat[k][i], max) > 0)
+                {
+                    max = mat[k][i];
+                    loc = k;
+                }
+            }
+
+            fraction temp;
+            for (int k = 1; k <= n + 1; k++)
+            {
+                temp = mat[i][k];
+                mat[i][k] = mat[loc][k];
+                mat[loc][k] = temp;
+            }
+            fraction factor = frdiv(mat[j][i], mat[i][i]);
+            for (int c = 1; c <= n + 1; c++)
+            {
+                fraction tfrac = frmul(factor, mat[i][c]);
+                mat[j][c] = frsub(mat[j][c], tfrac);
+            }
+        }
+    }
+
+    
+    return 1;
+}
+
+// Finds the sum of the numerators
+lint numerator_sum(int n, int power) {
+  lint sum = 0 ;
+  
+  for (int i = 1; i <= n; i++) {
+    sum += powl(i, power) ;
+  }
+  
+  return sum ;
+}
+
+// Creates a matrix for the fractions
+int make_matrix(fraction frac[MAX][MAX], int power) {
+  int i;
+  
+  for (int y = 1; y <= power; y++) {
+    for (int x = 1; x <= power; x++) {
+      int power = powl(y,x) ;
+      frac[y][x] = make_fraction(powl(y, x), 1) ;
+    }
+  }
+  
+  for (int y = 1; y <= p; y++) {
+    frac[y][power + 1] = make_fraction(numerator_sum(y, power - 1), 1) ;
+  }
+  printf("\n") ;
+  
+  triangularize(frac, power) ;
+  printMatrix(frac, power) ;
+  solveMatrix(frac, power) ;
+}
+
+// Does the main work of the function
 int main() {
+  fraction frac[MAX][MAX] ;
+  int num = 3 ;
+  int pow ;
+
+  printf("Enter the desired power: ") ;
+  scanf("%d", &pow) ;
+
   
 }
